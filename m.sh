18713fe7dadf837286f9ec84fff9115aca7eec54
Yijun Yu <y.yu@open.ac.uk>
2022-10-06 06:23:23 +0100

    update the script

--- m.sh
+++ m.sh
@@ -2,4 +2,4 @@
 if ! command -v tree-marker >/dev/null 2>&1; then
 	cargo install --git https://github.com/yijunyu/tree-grepper --branch patcher
 fi
-tokei --files --output=json | jq -r '.["Rust"].reports[].name' | parallel --bar tree-marker
+tokei --files --output=json | jq -r '.["Rust"].reports[].name' | parallel --bar tree-marker | sort | uniq > vocabulary.txt
