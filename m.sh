26d863f488ada950cb6063d19edc1f3e0931a619
Yijun Yu <y.yu@open.ac.uk>
2022-10-06 06:11:49 +0100

    use tokei to process the folder; leave out non-rust languages

--- m.sh
+++ m.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if ! command -v tree-patcher >/dev/null 2>&1; then
+if ! command -v tree-marker >/dev/null 2>&1; then
 	cargo install --git https://github.com/yijunyu/tree-grepper --branch patcher
 fi
-tokei --files --output=json | jq -r '.["Rust"].reports[].name' | xargs tree-patcher
+tokei --files --output=json | jq -r '.["Rust"].reports[].name' | xargs tree-marker
