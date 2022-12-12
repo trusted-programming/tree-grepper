54c078dd8b674b570a9256df6f529820437004af
Yijun Yu <y.yu@open.ac.uk>
2022-12-12 19:42:09 +0000

    split blocks

--- markup.sh
+++ markup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if ! command -v tree-marker >/dev/null 2>&1; then
-	cargo install --git https://github.com/yijunyu/tree-grepper --branch patcher
+	cargo install --git https://github.com/yijunyu/tree-grepper --branch unsafe_blocks
 fi
 tokei --files --output=json | jq -r '.["Rust"].reports[].name' | parallel --bar tree-marker | sort | uniq > vocabulary.txt
