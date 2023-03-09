06ce232d23642a84da0f1dfff81e52e9231ff4fe
Yijun Yu 00577373 <yijun.yu@huawei.com>
2023-03-09 18:35:20 +0800

    using tikv

--- components/persistence/src/lib.rs
+++ components/persistence/src/lib.rs
@@ -19,6 +19,32 @@ pub mod persistence {
     }
 }
 
+#[cfg(feature = "tikv")]
+pub mod persistence {
+    use tikv_client::RawClient;
+    use std::path::PathBuf;
+    use futures::executor::block_on;
+
+    pub fn get(key: PathBuf) -> tikv_client::Result<String> {
+        let v = async {
+            let client = RawClient::new(vec!["127.0.0.1:2379"]).await?;
+            let value = client.get(format!("{:?}",key)).await?;
+            let v = String::from_utf8(value.unwrap()).unwrap();
+            Ok(v)
+        };
+        block_on(v)
+    }
+    pub fn put(key: PathBuf, value: &str) -> tikv_client::Result<()> {
+        let v = async {
+            let client = RawClient::new(vec!["127.0.0.1:2379"]).await?;
+            client.put(format!("{:?}",key), value).await?;
+            Ok(())
+        };
+        block_on(v)
+    }
+}
+
+#[cfg(not(feature = "tikv"))]
 #[cfg(not(feature = "redis"))]
 pub mod persistence {
     use std::{result::Result, io::Error, path::PathBuf, fs::read_to_string};
@@ -41,11 +67,11 @@ mod tests {
     use crate::persistence;
 
     #[test]
-    fn main() {
+    fn main() -> tikv_client::Result<()> {
         let hello = Path::new("Hello").to_path_buf();
         let _ = persistence::put(hello.clone(), "World");
-        if let Ok(value) = persistence::get(hello) {
-            assert_eq!(format!("Hello, {value}!"), "Hello, World!");
-        }
+        let value = persistence::get(hello).unwrap();
+        assert_eq!(format!("Hello, {value}!"), "Hello, World!");
+        Ok(())
     }
 }
